a23d2468da52c828ac20529a0b98da28f98c1252
Alexander Kolov <me@alexkolov.com>
2014-07-02 15:17:34 +0200

    Update test project

---
+++ AXKViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXKViewLayout
+//
+//  Created by Alexander Kolov on 04/06/14.
+//  Copyright (c) 2014 Alexander Kolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
